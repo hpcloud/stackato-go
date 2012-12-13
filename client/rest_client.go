fbcbaaa1c5f36a1e31ab6ef17ae3664ad15b8714
Sridhar Ratnakumar <sridharr@activestate.com>
2012-12-13 14:25:12 -0800

    delegate http client api code to httpapi.client package

--- client/rest_client.go
+++ client/rest_client.go
@@ -1,19 +1,17 @@
 package client
 
 import (
-	"bytes"
 	"crypto/tls"
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"net/http"
+	"github.com/srid/httpapi/client"
 )
 
 type RestClient struct {
 	TargetURL    string
 	Token  string
 	Group  string
-	client *http.Client
+	client *client.Client
 }
 
 func NewRestClient(targetUrl, token, group string) *RestClient {
@@ -43,18 +41,8 @@ func (c *RestClient) CreateApp(name string) (int, error) {
 	return -1, err
 }
 
-// TODO: extract these into srid/httpapi
-
-// A version of http.NewRequest including token and group params; and taking path and params.
-func (c *RestClient) NewRequest(method string, path string, params map[string]interface{}) (*http.Request, error) {
-	url := c.TargetURL + path
-
-	body, err := json.Marshal(params)
-	if err != nil {
-		return nil, err
-	}
-
-	req, err := http.NewRequest(method, url, bytes.NewBuffer(body))
+func (c *RestClient) MakeRequest(method string, path string, params client.Hash) (client.Hash, error) {
+	req, err := client.NewRequest(method, path, params)
 	if err != nil {
 		return nil, err
 	}
@@ -62,47 +50,12 @@ func (c *RestClient) NewRequest(method string, path string, params map[string]in
 	if c.Group != "" {
 		req.Header.Set("X-Stackato-Group", c.Group)
 	}
-	return req, nil
-}
-
-func (c *RestClient) MakeRequest(method string, path string, params map[string]interface{}) (map[string]interface{}, error) {
-	req, err := c.NewRequest(method, path, params)
-	if err != nil {
-		return nil, err
-	}
-	resp, err := c.client.Do(req)
-	if err != nil {
-		return nil, err
-	}
-
-	return c.ParseResponse(resp)
-}
-
-func (c *RestClient) ParseResponse(resp *http.Response) (map[string]interface{}, error) {
-	defer resp.Body.Close()
-	data, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	var body map[string]interface{}
-	err = json.Unmarshal(data, &body)
-	if err != nil {
-		return nil, fmt.Errorf("Expecting JSON http response; %s", err)
-	}
-
-	// XXX: accept other codes
-	if !(resp.StatusCode == 200 || resp.StatusCode == 302) {
-		return nil, fmt.Errorf("HTTP request with failure code (%d); body -- %v",
-			resp.StatusCode, body)
-	}
-
-	return body, nil
+	return c.client.DoRequest(req)
 }
 
 // emulate `curl -k ...`
-func getInsecureHttpRestClient() *http.Client {
+func getInsecureHttpRestClient() *client.Client {
 	tr := &http.Transport{
 		TLSClientConfig: &tls.Config{InsecureSkipVerify: true}}
-	return &http.Client{Transport: tr}
+	return &client.Client{Transport: tr}
 }
